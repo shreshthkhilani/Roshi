247b5d49ba691a9060a120bac842105236f777e0
dhrupadb <dhrupadb@seas.upenn.edu>
2015-11-04 12:57:23 -0500

    Pushing sort of Node App

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var session = require('express-session');
 var async = require('async');
 var multer  = require('multer');
 var fs = require('fs');
-var secretobj = JSON.parse(fs.readFileSync('secret.json', 'utf8'));
+var secret = "abc-123";
 
-app.use(multer({dest: './uploads/', includeEmptyFields: true}));
+//app.use(multer({dest: './uploads/', includeEmptyFields: true}));
 app.set('port', process.env.PORT || 8080);
 app.engine('ejs', engine);
 app.set('views', path.join( __dirname, 'views'));
@@ -26,62 +26,20 @@ app.use(express.static(__dirname + '/views/js'));
 
 app.use(cookieParser());
 app.use(session({
-	secret: secretobj.secret,
+	secret: secret,
 	login: false
 }));
 
 /////////////////
 
-var aws = require("./keyvaluestore.js");
-
-var admins = new aws('admins');
-var hours = new aws('hours');
-var photos = new aws('photos');
-var count = new aws('count');
-var rejected = new aws('rejected');
-
-admins.init(function() {
-hours.init(function() {
-photos.init(function() {
-count.init(function() {
-rejected.init(function() {
-
-	routes.init(admins, hours, photos, count, rejected,
-		function() {
-		
-		app.get( '/', routes.index );
-		app.get( '/index.html', routes.index );
-		app.post( '/submitPhoto', routes.submitPhoto );
-
-		app.get( '/view', routes.view );
-		app.get( '/getGrid', routes.getGrid );
-		app.get( '/getCount', routes.getCount );
-
-		app.get( '/admin', routes.admin );
-		app.post( '/login', routes.login );
-
-		app.get( '/home', routes.home );
-		app.get( '/getQueue', routes.getQueue );
-		app.post( '/logout', routes.logout );
-		app.post( '/verifyPhoto', routes.verifyPhoto );
-
-		app.get( '/loggedIn', routes.loggedIn );
-
-		/////////////////////
-		
-		http.createServer( app ).listen( app.get( 'port' ), function(){
-			  console.log( 'Open browser to http://localhost:' + app.get( 'port' ));
-			} );
-		process.on('uncaughtException', function (err) {
-		  console.log('Caught exception: ' + err);
-		});
-
-		setTimeout(function () {
-		  console.log('This will still run.');
-		}, 500);
-	});
+//var aws = require("./keyvaluestore.js");
+routes.init(function() {
+	app.get( '/', routes.index );		
 });
+
+http.createServer( app ).listen( app.get( 'port' ), function(){
+	console.log( 'Open browser to http://localhost:' + app.get( 'port' ));
 });
-});
-});
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
 });
