e869b6ea251ef530af459d2fa64b7b92192e4246
dhrupadb <dhrupadb@seas.upenn.edu>
2015-11-04 14:13:16 -0500

    MongoDB server initialized

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var routes = require ('./routes');
 var http = require('http');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var app = express();
 var engine = require('ejs-locals');
 var bodyParser = require('body-parser');
@@ -29,6 +31,12 @@ app.use(session({
 	secret: secret,
 	login: false
 }));
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
 
 /////////////////
 
